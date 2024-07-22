397f7f8a7ed344a6306bd197623e3330e97dcddc
salhotra <nishantsalhotra.ns@gmail.com>
2024-07-22 17:12:18 +0530

    Update next config to export a static webpage

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * Set base path. This is a slug of the Github repo.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
   basePath: "/salhotra.github.io",
-  assetPrefix: "/salhotra.github.io/",
+
+  /**
+   * Disable server-based image optimization. Next.js doesn't support
+   * dynamic features for static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
