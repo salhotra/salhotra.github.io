97d45ec60927f1e3c32de40ba194b7e34809b219
salhotra <nishantsalhotra.ns@gmail.com>
2024-07-22 17:55:18 +0530

    Fix window reference error

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -36,11 +36,18 @@ const useContainerSize = (ref: React.RefObject<HTMLDivElement>) => {
 
 const useWindowSize = () => {
   const [windowSize, setWindowSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
+    width: 0,
+    height: 0,
   });
 
   useEffect(() => {
+    // Initial window size. This is done here instead of in the useState
+    // declaration because the window object is not available on the server.
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+
     const handleResize = () => {
       setWindowSize({
         width: window.innerWidth,
